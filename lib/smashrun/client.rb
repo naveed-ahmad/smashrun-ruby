3a5dd079c4df04c41abfcb6e05391f8cb5d81ad1
Naveed Ahmad <naveedahmada036@gmail.com>
2017-10-02 20:49:56 +0500

    fixed api urls and bump minor version

--- lib/smashrun/client.rb
+++ lib/smashrun/client.rb
@@ -16,51 +16,53 @@ module Smashrun
       @token.refresh!.to_hash
     end
     
+    # Retrieve latest activities
     def get_activities()
-      Oj.load(@token.get("v1/my/activities").body)
+      Oj.load(@token.get("/v1/my/activities").body)
     end
     
     # Filter activities, valid options are: *fromDateUTC*, *page* and *count*
     def search_activities(params={})
-      Oj.load(@token.get("v1/my/activities/search", params).body)
+      Oj.load(@token.get("/v1/my/activities/search", params).body)
     end
     
     # Returns the first 100 smashrun run ids.
     def latest_activities_ids
-      Oj.load(@token.get("v1/my/activities/search/ids").body)
+      Oj.load(@token.get("/v1/my/activities/search/ids").body)
     end
     
     # Returns an array of json objects containing the runId, startTime, distance (in kilometers), and duration
     def latest_activities_briefs
-      Oj.load(@token.get("v1/my/activities/search/briefs").body)
+      Oj.load(@token.get("/v1/my/activities/search/briefs").body)
     end
     
     # To retrieve a Google encoded polyline of a run route to easily draw a route map in Google maps.
     def activity_polyline(activity_id)
-      Oj.load(@token.get("v1/my/activities/#{activity_id}/polyline").body)
+      Oj.load(@token.get("/v1/my/activities/#{activity_id}/polyline").body)
     end
   
     # To retrieve an SVG polyline of a run route.
     def activity_svg_polyline(activity_id)
-      Oj.load(@token.get("v1/my/activities/#{activity_id}/polyline/svg").body)
+      Oj.load(@token.get("/v1/my/activities/#{activity_id}/polyline/svg").body)
     end
     
     # To retrieve a GeoJSON representation a run route.
     def activity_geojson(activity_id)
-      Oj.load(@token.get("v1/my/activities/#{activity_id}/polyline/geojson").body)
+      Oj.load(@token.get("/v1/my/activities/#{activity_id}/polyline/geojson").body)
     end
 
     # To retrieve a detail of single activity.
     def activity_detail(activity_id)
-      Oj.load(@token.get("v1/my/activities/#{activity_id}").body)
+      Oj.load(@token.get("/v1/my/activities/#{activity_id}").body)
     end
 
+    # Retrive activity splits
     def activity_splits(activity_id, unit)
       unless ['km', 'mi'].include?(unit)
-        raise "Invalid distance unit"
+        raise ArgumentError.new("Invlaid distance unit for splits. Valid units are km or mi")
       end
       
-      Oj.load(@token.get("v1/my/activities/#{activity_id}/splits/#{unit}").body)
+      Oj.load(@token.get("/v1/my/activities/#{activity_id}/splits/#{unit}").body)
     end
     
     def profile_info
